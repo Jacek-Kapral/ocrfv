87c484ee9082b139cd9fe6c7e1268392ca054790
Jacek-Kapral <jacek.kapral@proton.me>
2022-06-09 23:14:43 +0200

    Pierwsza wersja programu do robienia OCR z faktur oparta o Tesseract i OpenCV

---
+++ fvocr.py
@@ -0,0 +1,28 @@
+import pytesseract
+import cv2
+
+
+
+obraz = cv2.imread('fv.jpg')
+
+#konwersja obrazu na skale szarosci
+def skalaSzarosci(obraz):
+    return cv2.cvtColor(obraz, cv2.COLOR_BGR2GRAY)
+
+#usuniecie szumu z obrazu
+def usuwanieSzumu(obraz):
+    return cv2.medianBlur(obraz, 5)
+
+def progCzerni(obraz):
+    return cv2.threshold(obraz, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def ocrFv(obraz):
+    tekst = pytesseract.image_to_string(obraz)
+    return tekst
+
+
+obraz = skalaSzarosci(obraz)
+obraz = progCzerni(obraz)
+obraz = usuwanieSzumu(obraz)
+print(ocrFv(obraz))
+
